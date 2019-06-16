3604d7fde7a1b9a02a39dd86726437798327d396
王攀 <3052124905@qq.com>
2019-06-16 19:03:35 +0800

    frist init

---
+++ src/main/java/com/wp/bitcoinexploere/BitcoinexploereApplication.java
@@ -0,0 +1,13 @@
+package com.wp.bitcoinexploere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinexploereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinexploereApplication.class, args);
+    }
+
+}
